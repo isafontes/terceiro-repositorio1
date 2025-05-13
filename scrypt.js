954de7fb9916390ec4b76204475e650ab4551d70
isafontes <145460451+isafontes@users.noreply.github.com>
2025-05-13 11:36:28 -0300

    13/05

--- scrypt.js
+++ scrypt.js
@@ -14,11 +14,11 @@ document.addEventListener('DOMContentLoaded', function(){
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = '$(tamanhoAtualFonte)rem'
+        document.body.style.fontSize =  `$(tamanhoAtualFonte)rem`;
     })
 
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = '$(tamanhoAtualFonte)rem'
+        document.body.style.fontSize = `$(tamanhoAtualFonte)rem`;
     })
 })
